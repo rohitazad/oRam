5d875c1b63ad9ccddf8debea7148aec53af11126
rohitazad <ram9953878727@gmail.com>
2018-02-02 22:59:12 +0530

    SHopping cart item count normal code

--- src/app/oram/bs-navbar/bs-navbar.component.ts
+++ src/app/oram/bs-navbar/bs-navbar.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../service/auth-service.service';
 import { AppUser } from './../models/app-user';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { ShoppingCartService } from '../service/shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -12,17 +13,30 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class BsNavbarComponent implements OnInit {
   user:any[];
   appUser: AppUser;
+  shoppingCartItemCount:number;
 
-
-  constructor(public _authService : AuthService) { 
+  constructor(public _authService : AuthService,
+  private shoppingCartService:ShoppingCartService) { 
    // _authService.user$.subscribe( data => console.log(data));
    _authService.appUser$.subscribe(appUser => {
     this.appUser = appUser;
-    console.log(this.appUser);
+    //console.log(this.appUser);
+    
+
    });
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+
+      this.shoppingCartItemCount = 0;
+      for(let productId in cart.items){
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+        console.log(productId, cart.items[productId].quantity)
+      }
+    })
   }
 
   logOut(){
---
+++ src/app/oram/models/shopping-cart.ts
@@ -0,0 +1,14 @@
+import { ShoppingCartItem } from './shopping-cart-item';
+
+export class ShoppingCart{
+    items:ShoppingCartItem[];
+
+
+    get totalItemCount(){
+        let count = 0;
+        for(let productId in this.items){
+            count += this.items[productId].quantity;
+        }
+        return count;
+    }
+} 
